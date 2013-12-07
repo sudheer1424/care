e1cb32c9bb7197a2e3fc820a00080d4926e3df07
sudheer1424 <sudheerkumar1424@gmail.com>
2013-12-07 15:17:41 +0530

    Firest commat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Care::Application.config.secret_token = 'd47d7eda4e542907a601e18ff40d7841a6a6c79503c7d2d2aa9bebf2b78d59e125ca82e6005434fc2cbab9a794eecbdde1c14417407143278c04e9fae8a957d0'
